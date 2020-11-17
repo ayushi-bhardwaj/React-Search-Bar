6a91d1370012ca42697be1b32bd8b5f8a5d58f63
unknown <ayushibhardwaj288@gmail.com>
2020-11-17 12:45:47 +0530

    final commit

--- src/components/NameContainer.js
+++ src/components/NameContainer.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 class NameContainer extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.names.map(name=> <div>
-                    {name}
-                </div>)}
-            </div>
-        );
-    }
+  render() {
+    var list = this.props.names.map((name) => (
+      <li>
+        <FontAwesomeIcon icon={faSearch} />{" "}
+        <span className="left-span">
+          {name
+            .toLowerCase()
+            .substring(
+              0,
+              name.toLowerCase().indexOf(this.props.searchTerm.toLowerCase())
+            )}
+        </span>
+        <span className="middle-span">
+          {name
+            .toLowerCase()
+            .substring(
+              name.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()),
+              name.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) +
+                this.props.searchTerm.length
+            )}
+        </span>
+        <span className="right-span">
+          {name
+            .toLowerCase()
+            .substring(
+              name.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) +
+                this.props.searchTerm.length
+            )}
+        </span>
+      </li>
+    ));
+    return <div>{list}</div>;
+  }
 }
 
 export default NameContainer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,18 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger"
-import { persistStore, persistReducer } from 'redux-persist'
-import reducer from "../reducers/index"
-import storage from 'redux-persist/lib/storage'
-const persistConfig={
-    key: 'root',
-    storage
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import reducer from "../reducers/index";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-}
- 
-const persistedReducer = persistReducer(persistConfig, reducer)
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-// export function configureStore(){
-//     store=createStore(reducer,applyMiddleware(thunk, logger));
-
-//     return store;
-// }
 export default () => {
-    let store = createStore(persistedReducer,applyMiddleware(thunk, logger))
-    let persistor = persistStore(store)
-    return { store, persistor }
-  }
+  let store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
